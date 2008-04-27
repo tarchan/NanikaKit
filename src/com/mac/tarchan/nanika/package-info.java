06076a4d8f91b38ef544242029e71fc0b35e6472
tarchan <tarchan@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-27 12:09:20 +0000

    lang/java/NanikaKit: nanika パッケージの説明を追加
    git-svn-id: http://svn.coderepos.org/share/lang/java/NanikaKit/trunk@10594 d0d07461-0603-4401-acd4-de1884942a52

--- src/com/mac/tarchan/nanika/package-info.java
+++ src/com/mac/tarchan/nanika/package-info.java
@@ -9,6 +9,41 @@
 /**
  * ゴーストの実体化に使用するクラスのパッケージです。
  * 
+ * <h2>仕様書</h2>
+ * <ul>
+ * <li><a href="http://usada.sakura.vg/">うさださくら</a></li>
+ * <li><a href="http://www.geocities.jp/poskoma/">電気で動くうにゅう・廃屋の夏</a>
+ * <ul>
+ * <li><a href="http://www.geocities.jp/poskoma/docs/faq.html">ＦＡＱ</a></li>
+ * </ul></li>
+ * <li><a href="http://disc2.s56.xrea.com/manual/">Disc-2 ゴースト制作</a></li>
+ * </ul>
+ * 
+ * <h2>概要</h2>
+ * <ul>
+ * <li>NAR (アーカイブ)
+ * <ul>
+ * <li>GHOST (ゴースト)
+ * <ul>
+ * <li>SHIORI (AI的な何か)
+ * <ul>
+ * <li>MAKOTO (トランスレータ)</li>
+ * <li>SAORI (SHIORI 機能拡張)</li>
+ * <li>SHELL (外見)</li>
+ * </ul></li>
+ * <li>SURFACE (立ち絵)
+ * <ul>
+ * <li>MENU (メニュー)</li>
+ * <li>SERIKO (アニメーション)</li>
+ * </ul></li>
+ * <li>BALLOON (ふきだし)
+ * <ul>
+ * <li>SURFACE (ふきだし絵)</li>
+ * </ul></li>
+ * </ul></li>
+ * </ul></li>
+ * </ul>
+ * 
  * @since 1.0
  * @author tarchan
  */
