f618aeffab35842b3d0f0617457870723fb0725d
tarchan <tarchan@gmail.com>
2011-06-28 00:49:45 +0900

    不要なインポートを削除

--- src/com/mac/tarchan/nanika/shell/NanikaCanvas.java
+++ src/com/mac/tarchan/nanika/shell/NanikaCanvas.java
@@ -30,8 +30,6 @@ package com.mac.tarchan.nanika.shell;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
